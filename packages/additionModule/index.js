8ed49c391d1830e9e3aab655515d397b585fab97
ishant.oberoi <ishant.oberoi@nutanix.com>
2020-08-03 16:52:29 +0530

    first commit

---
+++ packages/additionModule/index.js
@@ -0,0 +1,4 @@
+module.exports = function add(a, b) {
+  console.log('Addition with yarn workspaces');
+  return a+b;
+}
---
+++ packages/consumerModule/index.js
@@ -0,0 +1,8 @@
+// Tutorial link https://www.youtube.com/watch?v=G8KXFWftCg0
+var add = require('additionModule');
+var substract = require('substractionModule');
+
+console.log('------------Consumer Module------------');
+console.log(`Addition a+b: ${add(4,5)}`);
+console.log(`Substraction a-b: ${substract(5,4)}`);
+console.log('---------------------------------------');
